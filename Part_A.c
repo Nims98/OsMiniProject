ad60257ca73984f918b969fd8d087af257a03713
Nims98 <nirmalasabaragamuwa@gmail.com>
2022-10-31 15:31:23 +0530

    Part_A init

---
+++ Part_A.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct student_marks
+{
+    char student_index[12]; // EG/XXXX/XXXX
+    float assgnmt01_marks;  // 15%
+    float assgnmt02_marks;  // 15%
+    float project_marks;    // 20%
+    float finalExam_marks;  // 50%
+};
+
+int main()
+{
+}
