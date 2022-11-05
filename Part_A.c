bd67b5876a986418a48330fea2a1433e421a4e2c
Nims98 <nirmalasabaragamuwa@gmail.com>
2022-11-05 18:53:08 +0530

    generate marks randomly

--- Part_A.c
+++ Part_A.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+// genereat the marks randomly
+// but should have the abulity to insert delete update manully
 
 struct student_marks
 {
@@ -11,4 +17,24 @@ struct student_marks
 
 int main()
 {
+    FILE *fp;
+    fp = fopen("marks.txt", "w");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "ERROR (%s:%d)\n -- %s", __FILE__, __LINE__, strerror(errno));
+        exit(1);
+    }
+
+    // genrate the marks randomly
+    struct student_marks marks;
+    for (int i = 0; i < 100; i++)
+    {
+        sprintf(marks.student_index, "EG/%04d/%04d", i, i);
+        marks.assgnmt01_marks = (float)(rand() % 100);
+        marks.assgnmt02_marks = (float)(rand() % 100);
+        marks.project_marks = (float)(rand() % 100);
+        marks.finalExam_marks = (float)(rand() % 100);
+
+        fwrite(&marks, sizeof(struct student_marks), 1, fp);
+    }
 }
