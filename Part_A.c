945bdf8d84ce250ccc78081eecd2f24cd6a57c12
Nims98 <nirmalasabaragamuwa@gmail.com>
2022-11-05 23:25:59 +0530

    added readme

--- Part_A.c
+++ Part_A.c
@@ -28,8 +28,6 @@ int main(int argc, char *argv[])
     // genrate the marks randomly
     if (argc == 1)
     {
-        /* code */
-
         struct student_marks marks;
         for (int i = 0; i < 100; i++)
         {
@@ -42,6 +40,7 @@ int main(int argc, char *argv[])
             fwrite(&marks, sizeof(struct student_marks), 1, fp);
         }
     }
+    // read recorded marks
     else if (argc == 2 && strcmp(argv[1], "-r") == 0)
     {
 
@@ -52,6 +51,7 @@ int main(int argc, char *argv[])
             printf("%s\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\n", marks.student_index, marks.assgnmt01_marks, marks.assgnmt02_marks, marks.project_marks, marks.finalExam_marks);
         }
     }
+    // insert marks
     else if (argc == 7 && strcmp(argv[1], "-i") == 0)
     {
         struct student_marks marks;
@@ -60,7 +60,54 @@ int main(int argc, char *argv[])
         marks.assgnmt02_marks = atof(argv[4]);
         marks.project_marks = atof(argv[5]);
         marks.finalExam_marks = atof(argv[6]);
+        fseek(fp, 0, SEEK_END);
         fwrite(&marks, sizeof(struct student_marks), 1, fp);
+        printf("Marks for %s inserted successfully\n", marks.student_index);
+    }
+    // update marks
+    else if (argc == 7 && strcmp(argv[1], "-u") == 0)
+    {
+        struct student_marks marks;
+        while (fread(&marks, sizeof(struct student_marks), 1, fp) == 1)
+        {
+            if (strcmp(marks.student_index, argv[2]) == 0)
+            {
+                if (strcmp(argv[3], "-") != 0)
+                    marks.assgnmt01_marks = atof(argv[3]);
+                if (strcmp(argv[4], "-") != 0)
+                    marks.assgnmt02_marks = atof(argv[4]);
+                if (strcmp(argv[5], "-") != 0)
+                    marks.project_marks = atof(argv[5]);
+                if (strcmp(argv[6], "-") != 0)
+                    marks.finalExam_marks = atof(argv[6]);
+                fseek(fp, -sizeof(struct student_marks), SEEK_CUR);
+                fwrite(&marks, sizeof(struct student_marks), 1, fp);
+                printf("Marks for %s updated successfully\n", marks.student_index);
+                break;
+            }
+        }
+    }
+    else if (argc == 3 && strcmp(argv[1], "-d") == 0)
+    {
+        // delete marks
+        struct student_marks marks;
+        while (fread(&marks, sizeof(struct student_marks), 1, fp) == 1)
+        {
+            if (strcmp(marks.student_index, argv[2]) == 0)
+            {
+                while (!feof(fp))
+                {
+                    fread(&marks, sizeof(struct student_marks), 1, fp);
+                    fseek(fp, -2 * sizeof(struct student_marks), SEEK_CUR);
+                    fwrite(&marks, sizeof(struct student_marks), 1, fp);
+                    fseek(fp, sizeof(struct student_marks), SEEK_CUR);
+                    break;
+                    // printf(marks.student_index);
+                }
+                printf("Marks for %s deleted successfully\n", marks.student_index);
+                break;
+            }
+        }
     }
 
     fclose(fp);
---
+++ README.md
@@ -0,0 +1,9 @@
+# Os Programming Project
+### This is an C Program developed to perform Insert, Update and Delete operations on file.
+## Insert
+#
+`` $ ./Part_A -i <INDEX> <ASSIGNMENT 1 MARKS> <ASSIGNMENT 1 MARKS> <PROJECT> <FINAL EXAM>``
+## Update
+#
+## Delete
+#
